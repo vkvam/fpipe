8536fcda68714216232e0d45add1381487b36efb
Vemund Kvam <vemund.kvam@get.no>
2020-01-06 14:30:38 +0100

    Remove useless try/except, errors should propagate

--- fpipe/gen/tar.py
+++ fpipe/gen/tar.py
@@ -11,27 +11,24 @@ from fpipe.meta.size import Size
 
 class Tar(MethodGen[FileStream, FileStream]):
     def executor(self, source: FileStream):
-        try:
-            with tarfile.open(
-                fileobj=source.file, mode="r|*"
-            ) as tar_content_stream:
-                for tar_info in tar_content_stream:
-                    tar_stream = tar_content_stream.extractfile(tar_info)
+        with tarfile.open(
+            fileobj=source.file, mode="r|*"
+        ) as tar_content_stream:
+            for tar_info in tar_content_stream:
+                tar_stream = tar_content_stream.extractfile(tar_info)
 
-                    if tar_stream:
-                        yield CallableResponse(
-                            FileStream(
-                                cast(BinaryIO, tar_stream),
-                                meta=(
-                                    Size(tar_info.size),
-                                    Modified(
-                                        datetime.datetime.fromtimestamp(
-                                            tar_info.mtime
-                                        )
-                                    ),
-                                    Path(tar_info.name),
+                if tar_stream:
+                    yield CallableResponse(
+                        FileStream(
+                            cast(BinaryIO, tar_stream),
+                            meta=(
+                                Size(tar_info.size),
+                                Modified(
+                                    datetime.datetime.fromtimestamp(
+                                        tar_info.mtime
+                                    )
                                 ),
-                            )
+                                Path(tar_info.name),
+                            ),
                         )
-        except (FileNotFoundError, tarfile.TarError):
-            raise
+                    )
